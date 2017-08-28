ccc594f5d5dae422b7e595af55225aff0bce0b46
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-08-28 23:39:55 +0000

    first commit

---
+++ test1.js
@@ -0,0 +1,16 @@
+
+//declaring javascript objects as variables
+
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+var motorBike = {
+
+  // Only change code below this line.
+  "wheels": 2,
+  "engines": 1,
+  "seats": 1
+};
