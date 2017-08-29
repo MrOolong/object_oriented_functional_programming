e4ffd745c9d29a4f828c6e83ee34de39e7b2b377
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-08-29 19:46:13 +0000

    Filter arrays with filter

---
+++ test8.js
@@ -0,0 +1,9 @@
+//Filter Arrays with filter
+
+var oldArray = [1,2,3,4,5,6,7,8,9,10];
+
+// Only change code below this line.
+
+var newArray = oldArray.filter(function(val) {
+  return val < 6;
+});
