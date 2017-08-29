84255aaf9979b4b31cef63f6e6b40e96dcaa235b
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-08-29 00:27:01 +0000

    Make Unique Objects by Passing Parameters to our Constructor

---
+++ test4.js
@@ -0,0 +1,12 @@
+//Make Unique Objects by Passing Parameters to our Constructor
+
+
+var Car = function(wheels, seats, engines) {
+  //Change this constructor
+  this.wheels = wheels;
+  this.seats = seats;
+  this.engines = engines;
+};
+
+//Try it out here
+var myCar = new Car(3, 1, 1);
